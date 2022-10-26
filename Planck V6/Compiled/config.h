bcdc0f0277eb5b8f909729d35f70338b544a56a6
Joe Scotto <contact@joescotto.com>
2022-10-25 22:28:29 -0400

    Add songs

--- Planck V6/Compiled/config.h
+++ Planck V6/Compiled/config.h
@@ -17,13 +17,43 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define ALL_STAR_CUSTOM \
+    Q__NOTE(_F4),      \
+    Q__NOTE(_G4),      \
+    Q__NOTE(_BF4),     \
+    Q__NOTE(_G4),      \
+    HD_NOTE(_D5),      \
+    HD_NOTE(_D5),      \
+    W__NOTE(_C5),      \
+    S__NOTE(_REST),    \
+    Q__NOTE(_F4),      \
+    Q__NOTE(_G4),      \
+    Q__NOTE(_BF4),     \
+    Q__NOTE(_G4),      \
+    HD_NOTE(_C5),      \
+    HD_NOTE(_C5),      \
+    W__NOTE(_BF4),     \
+    S__NOTE(_REST),    \
+    Q__NOTE(_F4),      \
+    Q__NOTE(_G4),      \
+    Q__NOTE(_BF4),     \
+    Q__NOTE(_G4),      \
+    W__NOTE(_BF4),     \
+    H__NOTE(_C5),      \
+    H__NOTE(_A4),      \
+    H__NOTE(_A4),      \
+    H__NOTE(_G4),      \
+    H__NOTE(_F4),      \
+    H__NOTE(_F4),      \
+    W__NOTE(_C5),      \
+    W__NOTE(_BF4),
+
 // Define options
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_TERM 135
 #define PERMISSIVE_HOLD
 #define TAPPING_TERM_PER_KEY
 #define AUDIO_CLICKY
-// #define AUDIO_CLICKY_FREQ_RANDOMNESS 1.0f
-#define AUDIO_CLICKY_FREQ_MAX 10000.0f
-
-#define AUDIO_CLICKY_FREQ_DEFAULT 1500.0f
+#define AUDIO_CLICKY_FREQ_RANDOMNESS 1.0f
+#define AUDIO_INIT_DELAY
+#define STARTUP_SONG SONG(ALL_STAR_CUSTOM)
